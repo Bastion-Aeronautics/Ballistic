c09d6819792137351779be774b1c08149fa68701
Savro <savro1248@gmail.com>
2026-05-10 15:12:39 +1000

    added trajectory point list

--- HTTP/testing/debug_server.py
+++ HTTP/testing/debug_server.py
@@ -3,12 +3,14 @@ from flask import Flask, request
 import logging
 app = Flask('app')
 
+trajectory = [] # This list will store the trajectory points
 
 @app.route('/updatePosition') # This function can be called to update the position
 def updatePosition():
     current_x = request.args.get('x')
     current_y = request.args.get('y')
     current_z = request.args.get('z')
+    trajectory.append((float(current_x), float(current_y), float(current_z))) # Store the position as a tuple in the trajectory list
     print(f"Position: {float(current_x):.0f}, {float(current_y):.0f}, {float(current_z):.0f}")
     return "OK"
 
